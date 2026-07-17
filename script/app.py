585003a02489df66e6e726338db396ca362ec8b4
Navdeep <navshra680@gmail.com>
2026-07-17 14:38:44 +0530

    perf: limit initial read_csv row ingestion to prevent cloud OOM crash

--- script/app.py
+++ script/app.py
@@ -14,7 +14,7 @@ st.write("---")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/cleaned_customer_data.csv")
+    return pd.read_csv("data/cleaned_customer_data.csv", nrows=100000)
 
 df = load_data()
 
